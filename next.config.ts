b9684cc515b45b2db061b022eb57f8d2aa4314d3
Jackal_lzt <bkyarokun@gmail.com>
2024-11-01 16:58:37 +0800

    feat: chapter10 - Partial prerendering(experimental)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	experimental: {
+		// 启动partial prerednering
+		// ppr: "incremental",
+	},
 };
 
 export default nextConfig;
